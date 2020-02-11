ec9d2e7faedacb68e50c8bcf9bc84d75b3cacf2d
Ivo Mota <ivo.mota@farfetch.com>
2020-02-11 14:28:48 +0000

    Fix theme deploy - test 4

--- src/deploy/index.js
+++ src/deploy/index.js
@@ -9,8 +9,19 @@ const GhostAdminApi = require('@tryghost/admin-api');
       version: 'canary'
     });
 
+    const basePath = process.env.GITHUB_WORKSPACE;
+    const pkgPath = path.join(process.env.GITHUB_WORKSPACE, 'package.json');
+    const themeName = core.getInput('theme-name') || require(pkgPath).name;
+    const themeZip = `${themeName}.zip`;
+    const zipPath = path.join(basePath, themeZip);
+    const exclude = core.getInput('exclude') || '';
+
+    // Create a zip
+    await exec.exec(`zip -r ${themeZip} . -x *.git* *.zip yarn* npm* *routes.yaml *redirects.yaml *redirects.json ${exclude}`, [], {cwd: basePath});
+
     // Deploy it to the configured site
-    await api.themes.upload({file: '../../liebling-themindsetbox.zip'});
+    await api.themes.upload({file: zipPath});
+    // await api.themes.upload({file: '../../liebling-themindsetbox.zip'});
     console.log('Theme successfully uploaded.');
   } catch (err) {
     console.error(err);
