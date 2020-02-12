e875eb13f9eef42f877e5e3e0a27406c302fbd2b
Ivo Mota <ivo.mota@farfetch.com>
2020-02-12 09:10:10 +0000

    Fix theme deploy - test 12

--- src/deploy/index.js
+++ src/deploy/index.js
@@ -1,16 +1,16 @@
-// require('dotenv').config()
+require('dotenv').config()
 const GhostAdminApi = require('@tryghost/admin-api');
 
 (async function main() {
   try {
     const api = new GhostAdminApi({
-      url: process.env.url,
-      key: process.env.key,
+      url: process.env.GHOST_ADMIN_API_URL,
+      key: process.env.GHOST_ADMIN_API_KEY,
       version: 'canary'
     });
 
     // Deploy it to the configured site
-    await api.themes.upload({file: './src/liebling-themindsetbox.zip'});
+    await api.themes.upload({file: '../liebling-themindsetbox.zip'});
     console.log('Theme successfully uploaded.');
   } catch (err) {
     console.error(err);
